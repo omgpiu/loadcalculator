577a3a22265b1ff5b20af9f293bc0da6cf8cb52a
omgpiu <dyba@list.ru>
2021-01-30 11:58:41 +0300

    Css

--- src/test/t2-pages/p1-stepOne/pageOne.tsx
+++ src/test/t2-pages/p1-stepOne/pageOne.tsx
@@ -10,8 +10,6 @@ export const PageOne: React.FC = () => {
     return (
         <div className={st.wrapper}>
             <div className={st.main}>
-
-
                 <div>
                     <img src={container} alt="container-logo"/>
                     <InputPage type='checkbox'/>
@@ -20,9 +18,7 @@ export const PageOne: React.FC = () => {
                     <img src={truck} alt="truck-logo"/>
                     <InputPage type='checkbox' name='Импорт'/>
                 </div>
-
             </div>
-
             <div className={st.buttonBox}>
                 <ButtonPage name='Импорт'/>
                 <ButtonPage name='Следующий шаг'/>
