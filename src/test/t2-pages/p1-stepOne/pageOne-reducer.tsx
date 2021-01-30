b85aea7cefa748187d922788ba5ab0a046ed39ff
omgpiu <dyba@List.ru>
2021-01-30 20:03:32 +0300

    PageOne finish

--- src/test/t2-pages/p1-stepOne/pageOne-reducer.tsx
+++ src/test/t2-pages/p1-stepOne/pageOne-reducer.tsx
@@ -1,1 +1,17 @@
-export default 1;
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+const initialState = {};
+
+
+const slice = createSlice({
+        name: 'pageOne',
+        initialState,
+        reducers: {
+            setLoadType(state, action: PayloadAction<{ type: string }>) {
+                
+            }
+        }
+    })
+;
+export const pageOneReducer = slice.reducer;
+
