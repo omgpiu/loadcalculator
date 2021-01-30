ffe8441802b5aef8ad542f14cf6ce4774f926803
omgpiu <dyba@List.ru>
2021-01-30 10:27:06 +0300

    Common Button and Input

--- src/test/t2-pages/p1-stepOne/pageOne.tsx
+++ src/test/t2-pages/p1-stepOne/pageOne.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-
-
+import st from './pageOne.module.css';
+import {ButtonPage} from '../../t5-common/c-1-btn/ButtonPage';
+import {InputPage} from '../../t5-common/с-2-inp/InputPage';
 
 
 export const PageOne: React.FC = () => {
     return (
-        <div>
-            PageOne
+        <div className={st.wrapper}>
+
+            <InputPage type='checkbox'/>
+            <InputPage type='checkbox' name='Импорт'/>
+            <ButtonPage name='Импорт'/>
+            <ButtonPage name='Следующий шаг'/>
+
 
         </div>
     );
