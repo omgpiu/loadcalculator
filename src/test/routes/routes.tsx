35077fe0a356636a08a67cf5a2d2f0a0055859eb
omgpiu <dyba@List.ru>
2021-01-30 22:35:54 +0300

    PageOne reducer test + PageTwo init reducer

--- src/test/routes/routes.tsx
+++ src/test/routes/routes.tsx
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 import {Login} from '../t1-login/login';
 import {Page5Pallets} from '../t2.1-pages/p5-pallets/p5-pallets';
 import {PageOne} from '../t2-pages/p1-stepOne/pageOne';
+import {PageTwo} from '../t2-pages/p2-stepTwo/pageTwo';
 
 
 export const LOGIN = '/login';
@@ -19,6 +20,7 @@ export const Routes = () => {
             <Route path={LOGIN} render={Login}/>
 
             <Route exact path={PAGE_ONE} render={() => <PageOne/>}/>
+            <Route path={PAGE_TWO} render={() => <PageTwo/>}/>
             <Route path={PALLETS} render={() => <Page5Pallets/>}/>
 
 
