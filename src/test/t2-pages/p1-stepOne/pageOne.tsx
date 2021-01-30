c319f07610a4661b9a4e201e6992a8a869338d43
omgpiu <dyba@List.ru>
2021-01-30 10:57:05 +0300

    css

--- src/test/t2-pages/p1-stepOne/pageOne.tsx
+++ src/test/t2-pages/p1-stepOne/pageOne.tsx
@@ -2,16 +2,32 @@ import React from 'react';
 import st from './pageOne.module.css';
 import {ButtonPage} from '../../t5-common/c-1-btn/ButtonPage';
 import {InputPage} from '../../t5-common/с-2-inp/InputPage';
+import container from '../../../assets/images/container.jpg';
+import truck from '../../../assets/images/truck.jpg';
 
 
 export const PageOne: React.FC = () => {
     return (
         <div className={st.wrapper}>
+            <div className={st.main}>
 
-            <InputPage type='checkbox'/>
-            <InputPage type='checkbox' name='Импорт'/>
-            <ButtonPage name='Импорт'/>
-            <ButtonPage name='Следующий шаг'/>
+
+                <div>
+                    <img src={container} alt="container-logo"/>
+                    <InputPage type='checkbox'/>
+                </div>
+                <div>
+                    <img src={truck} alt="truck-logo"/>
+                    <InputPage type='checkbox' name='Импорт'/>
+                </div>
+
+            </div>
+
+            <div className={st.buttonBox}>
+                <ButtonPage name='Импорт'/>
+                <ButtonPage name='Следующий шаг'/>
+
+            </div>
 
 
         </div>
