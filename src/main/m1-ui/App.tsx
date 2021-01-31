2c27b74831ae116db2e793d2e8cfca294340ae3d
omgpiu <dyba@List.ru>
2021-01-31 09:54:53 +0300

    reducer minor fix

--- src/main/m1-ui/App.tsx
+++ src/main/m1-ui/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
 import {Calculator} from '../../test/calculator/Calculator';
-import './App.css'
-import {Layout, Menu, Steps} from 'antd';
-import {PageOne} from '../../test/t2-pages/p1-stepOne/pageOne';
+import {Layout, Steps} from 'antd';
 
 const {Header, Content, Footer, Sider} = Layout;
 const {Step} = Steps;
@@ -13,14 +11,13 @@ const App = () => {
     return (
 
 
-
-        <Layout style={{minHeight: '100vh',backgroundColor:'white'}}>
-            <Sider >
-                    <Steps direction="vertical" current={1}>
-                        <Step title="Finished" description="This is a description."/>
-                        <Step title="In Progress" description="This is a description."/>
-                        <Step title="Waiting" description="This is a description."/>
-                    </Steps>
+        <Layout style={{minHeight: '100vh', backgroundColor: 'white'}}>
+            <Sider>
+                <Steps direction="vertical" current={1}>
+                    <Step title="Finished" description="This is a description."/>
+                    <Step title="In Progress" description="This is a description."/>
+                    <Step title="Waiting" description="This is a description."/>
+                </Steps>
             </Sider>
             <Layout className="site-layout">
                 <Header className="site-layout-background" style={{padding: 0}}/>
@@ -33,6 +30,6 @@ const App = () => {
             </Layout>
         </Layout>
     );
-}
+};
 
 export default App;
