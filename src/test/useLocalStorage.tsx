2b6b5ad7e2a180d30485778578082093b84086c5
Seynaro <xamzatseynaro@gmail.com>
2021-02-08 15:21:51 +0300

    add useLocalStorage

---
+++ src/test/useLocalStorage.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+
+export const useLocalStorage = (key: any, initialValue: any) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            const item = window.localStorage.getItem(key); //
+            return item ? JSON.parse(item) : initialValue; //
+        } catch (err) {
+            console.error(err);
+            return initialValue;
+        }
+    });
+
+    const setValue = (value: any) => {
+        try {
+            const valueToStore =
+                value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            window.localStorage.setItem(key, JSON.stringify(valueToStore)); //
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return [storedValue, setValue];
+};
+
+// const [storedPage, setPage] = useLocalStorage("page", "PAGE_ONE") // сам хук
+// const page = storedPage      - получение данных из Local Storage
+// setPage(page)                - сохранение данных в Local Storage
+
+
