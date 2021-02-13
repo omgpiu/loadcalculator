9dcd63e9699416f769080d49fcd3c561f4afacbd
omgpiu <dyba@List.ru>
2021-02-13 21:04:18 +0300

    LoginPage UI add comment warning antd

--- src/test/t1-login/login.tsx
+++ src/test/t1-login/login.tsx
@@ -5,12 +5,10 @@ import st from './Login.module.css';
 import {EyeInvisibleOutlined, EyeTwoTone, LockOutlined, UserOutlined} from '@ant-design/icons/lib/icons';
 import {Link, Redirect} from 'react-router-dom';
 import {getCaptcha, getError, getIsAuth} from './login-selectors';
-import {PAGE_ONE, PAGE_THREE} from '../routes/routes';
+import {PAGE_ONE} from '../routes/routes';
 import ButtonBlock from '../t5-common/buttonBlock/buttonBlock';
 
 export const Login = () => {
-
-
 
 
     const error = useSelector(getError);
@@ -34,87 +32,86 @@ export const Login = () => {
     };
     return (
         <>
-        <Form onFinish={onSubmit}
+            <Form onFinish={onSubmit}
                   className={st.loginForm}
-        >
-            <Form.Item
-                name="email"
-                {...error && {
-                    help: error,
-                    validateStatus: 'error',
-                }}
-                rules={[
-                    {
-                        type: 'email',
-                        message: 'The input is not valid E-mail!',
-                    },
-                    {
-                        required: true,
-                        message: 'Please input your Email!',
-                    },
-                ]}
             >
-                <Input prefix={<UserOutlined className="site-form-item-icon"/>}
-                       type="email"
-                       name="email"
-                       placeholder="Email"
-                       onClick={resetError}
-                />
-            </Form.Item>
-            <Form.Item
-                name="password"
-                {...error && {
-                    help: error,
-                    validateStatus: 'error'
-                }}
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Password!',
-                    },
-
-                ]}
-            >
-                <Input.Password
-                    prefix={<LockOutlined className="site-form-item-icon"/>}
-                    placeholder="Password"
-                    iconRender={visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
-                    onClick={resetError}
-                />
-            </Form.Item>
-            <Form.Item>
                 <Form.Item
-                    name="remember"
-                           valuePropName="checked" noStyle>
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-                <Link target={'_blank'} className={st.loginFormForgot} to="https://social-network.samuraijs.com/login">
-                    Forgot password
-                </Link>
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className={st.loginFormButton}
+                    name="email"
+                    {...error && {
+                        help: error,
+                        validateStatus: 'error',
+                    }}
+                    rules={[
+                        {
+                            type: 'email',
+                            message: 'The input is not valid E-mail!',
+                        },
+                        {
+                            required: true,
+                            message: 'Please input your Email!',
+                        },
+                    ]}
                 >
-                    Log in
-                </Button>
-                Or <Link target={'_blank'} to="https://social-network.samuraijs.com/signUp">register now!</Link>
+                    <Input prefix={<UserOutlined className="site-form-item-icon"/>}
+                           type="email"
+                           name="email"
+                           placeholder="Email"
+                           onClick={resetError}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    {...error && {
+                        help: error,
+                        validateStatus: 'error'
+                    }}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Password!',
+                        },
+
+                    ]}
+                >
+                    <Input.Password
+                        prefix={<LockOutlined className="site-form-item-icon"/>}
+                        placeholder="Password"
+                        iconRender={visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
+                        onClick={resetError}
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked" noStyle>
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+                    <Link target={'_blank'} className={st.loginFormForgot}
+                          to="https://social-network.samuraijs.com/login">
+                        Forgot password
+                    </Link>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className={st.loginFormButton}
+                    >
+                        Log in
+                    </Button>
+                    Or <Link target={'_blank'} to="https://social-network.samuraijs.com/signUp">register now!</Link>
 
 
-            </Form.Item>
+                </Form.Item>
 
-            <img src={captchaUrl ? captchaUrl : undefined} alt=""/>
-            <Form.Item
-                // name="captcha"
-            >
-                {captchaUrl && <Input
-                    placeholder="captcha"
-                    onClick={resetError}
-                    style={{width: '100px'}}
-                />}
-
-            </Form.Item>
-
-        </Form>
+                <img src={captchaUrl ? captchaUrl : undefined} alt=""/>
+                <Form.Item
+                    // name="captcha" пофиксить капчу
+                >
+                    {captchaUrl && <Input
+                        placeholder="captcha"
+                        onClick={resetError}
+                        style={{width: '100px'}}
+                    />}
+                </Form.Item>
+            </Form>
             {/*Для теста*/}
             <ButtonBlock type={'default'} prevPageLink={PAGE_ONE}/>
         </>
