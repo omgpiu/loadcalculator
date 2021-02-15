6541b0d0fd7c8e4aadf83228be5e681e432a1e4d
Khamzat <xamzatseynaro@gmail.com>
2021-02-15 22:31:53 +0300

    add receiving packagingCargo

--- src/test/t2-pages/p5-stepFive/CargoPage.tsx
+++ src/test/t2-pages/p5-stepFive/CargoPage.tsx
@@ -1,17 +1,13 @@
 import {CargoModule} from "./CargoModule";
 import React from "react";
 import st from './PageFive.module.css'
-import { getPackagingCargo } from "../p2-stepTwo/pageTwo-selector";
-import { useSelector } from "react-redux";
+import {getPackagingCargo} from "../p2-stepTwo/pageTwo-selector";
+import {useSelector} from "react-redux";
 
 
 export const CargoPage = () => {
 
 	const packagingCargo = useSelector(getPackagingCargo)
-
-	const cargoDescription2 = ['Груз: Груз 2', 'Количество: 80 штук', 'Вес: 50 кг.', 'Длина: 600 мм.',
-		'Ширина: 400 мм.', 'Высота: 200 мм.']
-
 
 	return <div className={st.cargoPage}>
 
